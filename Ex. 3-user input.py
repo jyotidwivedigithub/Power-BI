7d94dd345874666401a05a74ffe531bcf9dc4661
Jyoti Dwivedi <124238555+jyotidwivedigithub@users.noreply.github.com>
2023-04-26 00:25:19 +0530

    Add files via upload

---
+++ Ex. 3-user input.py
@@ -0,0 +1,16 @@
+#Ex.3-User input using Arithmatic function
+a=int(input("Enter first value: "))
+b=int(input("Enter second value: "))
+
+sum=(a+b)
+print("Sum of a and b:", a+b)
+
+substraction=(a-b)
+print("Difference of a and b:",  a-b)
+
+multiplication=(a*b)
+print("Multiplication of a and b:", a*b)
+
+divison=(a/b)
+print("divison of a and b:", a/b)
+
