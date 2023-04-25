b690ad28f77da2b2fd7ab0961ac6f18ab38688b1
Jyoti Dwivedi <124238555+jyotidwivedigithub@users.noreply.github.com>
2023-04-26 00:42:40 +0530

    Add files via upload

---
+++ No. System(Binary,Decimal,Octa,HexaDecimal).py
@@ -0,0 +1,21 @@
+#three type of no--no system is used to creat a number
+# 1.-Integer...
+ #Binary(base2)..Decimal(base10)..Octa(base8)..Hexadecimal(base16)
+# 2.-Float...
+# 3.-Complex...
+
+# INTEGER
+#1.-Binary Number           binary has base-2 (2 digit 0,1)...0-1    BIT-BInary DigiT
+print(bin(31))
+
+#2.-Decimal Number          decimal has base-10 (10 digit 0,1,2,3,4,5,6,7,8,9)...0-9
+print(0b0101)                 #converting binary to decimal no.
+
+#3.-Octa Number             octa has base-8 (8 digit 0,1,2,3,4,5,6,7)....0-8
+print(oct(64))
+
+#4.-Hexa  Decimal           Hexa has base-16 (16 digit 0,1,2,3,4,5,6,7,8,9,a-f)
+print(hex(24))
+print(hex(10))
+
+
