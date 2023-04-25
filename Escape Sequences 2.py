1ee3e982dd4b326fa7ed08740c7f0f1d2f95d452
Jyoti Dwivedi <124238555+jyotidwivedigithub@users.noreply.github.com>
2023-04-26 00:23:02 +0530

    Add files via upload

---
+++ Escape Sequences 2.py
@@ -0,0 +1,8 @@
+#\v Vertical tab
+#\xhh Character with hex value hh (exactly 2 digits)
+#\ooo Character with octal value ooo (up to 3 digits)
+#\0 Null:binary 0 character (doesn't end string)
+#\N {id} Unicode database ID
+#\uhhhh Unicode Unicode character with 16-bit hex value 
+#\Uhhhhhhhh Unicode character with 32-bit hex valuea
+#\other Not an escape (keeps both \ and other)
