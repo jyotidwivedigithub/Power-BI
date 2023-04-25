52f51234ab0a9fa96722c2334157f4be8ece6eb0
Jyoti Dwivedi <124238555+jyotidwivedigithub@users.noreply.github.com>
2023-04-26 00:57:03 +0530

    Add files via upload

---
+++ Typecasting.py
@@ -0,0 +1,13 @@
+#explicit conversion (explicit type casting)
+a=int(3)
+b=int(5)
+print(a+b)
+
+c=str("hema")
+d=str(" malini")
+print(c+d)
+
+#implicit conversion (implicit type casting)
+e=4
+f=4.6
+print(e+f)       #it convert itself integer into floating/ floating into integer
