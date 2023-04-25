bbf7a92f0952c21a0efa2e2e17a510701acdce69
Jyoti Dwivedi <124238555+jyotidwivedigithub@users.noreply.github.com>
2023-04-26 00:54:57 +0530

    Add files via upload

---
+++ Sum.py
@@ -0,0 +1,29 @@
+#Integer (int.)
+a=4
+b=4
+sum=a+b
+print("the sum of both number is: ", sum)
+
+#float   (float..)
+a=34.34
+b=30.5
+sum=a+b
+print("the sum of both number is:", sum )
+
+#sting   (str.)
+a="madhu"        
+b=" tiwari"
+sum=a + b
+print("the sum of both number is:", sum)
+
+#int.+float
+a=4
+b=3.4
+sum=a+int(b)
+print("the sum of both number is:", sum)
+
+#int.+str.
+a=4
+b="ram"      #error...becoz "int+string "is not possible
+sum=a+int(b)
+print("the sum of both number is:", sum)
