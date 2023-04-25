2133238efcb0b018369ac898bda74c6668155b15
Jyoti Dwivedi <124238555+jyotidwivedigithub@users.noreply.github.com>
2023-04-26 00:38:40 +0530

    Add files via upload

---
+++ Nested-if.py
@@ -0,0 +1,13 @@
+num=18
+if(num < 0):
+    print("no.is negative")
+elif(num > 0):
+    if(num<=10):
+        print("no.is between 1-10")
+    elif(num>10 and num<=20):
+        print("no. is between 11-20")
+    else:
+        print("no.is greater then 20")
+else:
+    print("no.is zero")
+
