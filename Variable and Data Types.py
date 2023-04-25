b0269101571e33ef96fa04f481937e12932b8fb3
Jyoti Dwivedi <124238555+jyotidwivedigithub@users.noreply.github.com>
2023-04-26 00:57:53 +0530

    Add files via upload

---
+++ Variable and Data Types.py
@@ -0,0 +1,32 @@
+# Literals / Values / Constant / Data Types   are 4 types
+# 1-Numbers  2-Boolean(True/False)   3-None   4-String(" ")
+# Numbers:- 1-Integer 2-Float  3-Complex
+   #Integer:- 1-Decimal(2,1.3,5)   2-Octal(0o25)   3-HexaDecimal(0xA12)   4-Binary(0b1010)
+
+# Numerical Data Type
+a=3456              #integer
+print(a)
+
+b=23.45            #Floating
+print(b)
+
+c=complex(2, 3)     #complex
+print(c)
+
+#Text data type
+d="Harshita"        #string
+print(d)
+
+#boolean data type
+e=True              #boolean
+print(e)
+
+#none data type
+f=None              #none
+print(f)
+print("type of a is", type(a))
+print("type of b is", type(b))
+print("type of c is", type(c))
+print("type of d is", type(d))
+print("type of e is", type(e))
+print("type of f is", type(f))
