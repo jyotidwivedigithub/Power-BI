96ebe4ae9e5995fd17475fdc84fe3d19ed9a04c1
Jyoti Dwivedi <124238555+jyotidwivedigithub@users.noreply.github.com>
2023-04-26 00:49:29 +0530

    Add files via upload

---
+++ Separator $ end.py
@@ -0,0 +1,6 @@
+#separation of two character by using sep="~"
+print("hey", 6, 7, sep="~")
+#what to print in the end using end="\n"
+#we want to print at the end of 002...... "hey~4~5~oo2",  
+print("hey" , 4,5, sep="~", end="002\n")
+
