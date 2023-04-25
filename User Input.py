ccd247ff9d6a8d9eff1f4006ead032b8c675a480
Jyoti Dwivedi <124238555+jyotidwivedigithub@users.noreply.github.com>
2023-04-26 00:57:22 +0530

    Add files via upload

---
+++ User Input.py
@@ -0,0 +1,19 @@
+a=input()
+print(a)
+
+#input function
+b=input("Enter your name :  ")
+print("my name is", b)
+
+x=input("Enter first number: ")      #12
+y=input("Enter second number: ")          #3
+print(int(x) +int(y))                #15
+ 
+print(x+y)            #it will give 12+3=123  which is called concatenate
+
+c=input("enter your first no: ")
+d=input("enter your second no: ")
+print(int(c) + float(d))
+
+
+
