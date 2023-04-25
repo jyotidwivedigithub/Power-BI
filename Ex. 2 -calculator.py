cffbc240139bbcc1653333c5936005bc8ff6b6ff
Jyoti Dwivedi <124238555+jyotidwivedigithub@users.noreply.github.com>
2023-04-26 00:24:10 +0530

    Add files via upload

---
+++ Ex. 2 -calculator.py
@@ -0,0 +1,19 @@
+## Exercise 1 -Create a calculator
+ #Create a calculator capable of performing addition, substraction, multiplication, divison operations on two numbers. Your program should format the output in a readable manner.
+
+a=40
+b=5
+print("the value of", a, "+", b, "is", a+b)
+print("the value of", a, "-", b, "is", a-b)
+print("the value of", a, "*", b, "is", a*b)
+print("the value of", a, "/", b, "is", a/b)
+#or
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+#or
+print('the value of', a+b)
+print("the value of", a-b)
+print("the value of", a*b)
+print("the value of", a/b)
