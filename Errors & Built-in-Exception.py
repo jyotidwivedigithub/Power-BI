aea3d54758f8d85c28041f384b554785bf621b63
Jyoti Dwivedi <124238555+jyotidwivedigithub@users.noreply.github.com>
2023-04-26 00:22:32 +0530

    Add files via upload

---
+++ Errors & Built-in-Exception.py
@@ -0,0 +1,23 @@
+# These errors can be broadly classified into two classes:
+  #1.-Syntax Errors                   Syntax errors can not be handled....syntax always should be right to handle
+  #2.-Logical Errors(Exeptions)       it can be handle
+  
+
+  #1.-Syntax Errors
+a=10
+b=20
+#if a==b      #it is error syntax because we have not use colon : (if a==b:) so we have to correct mistake and put colon a==b:
+if a==b:
+  print("yes")
+#else         #it is error syntax because we have not use colon : (else:) so we have to correct mistake and put colon else:
+else:
+  print("no")
+
+
+#2.-Logical Errors (Exceptions)
+
+#print(1/0)            #Zero Division Error:division b zero
+l=[10,20,30,40]
+#print(l[6])           # Index Error)  
+print(l)
+
