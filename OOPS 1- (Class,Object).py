040b4cd53db2cc3f2e173523b1192e1e5c5d1614
Jyoti Dwivedi <124238555+jyotidwivedigithub@users.noreply.github.com>
2023-04-26 00:43:23 +0530

    Add files via upload

---
+++ OOPS 1- (Class,Object).py
@@ -0,0 +1,16 @@
+# Object Oriented Programming..(Class,Object concept defined under oops)
+  #Class - we can define variable and method/function in class..
+  #Object - object is used to call function defined in class
+
+class DemoClass:
+    a=10
+    def sumvalue(self):
+        print(20+40)
+
+demoobject=DemoClass()
+demoobject1=DemoClass()
+print(demoobject.a)
+print(demoobject1.a)
+demoobject.sumvalue();
+
+
